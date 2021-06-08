0e7bf6274cb643e3b79bc8f47c7b5b0957d59120
gtsiqueira <gildotsiqueira@gmail.com>
2021-06-08 00:53:50 -0300

    test in the person

---
+++ src/main/java/com/gts/personapi/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.gts.personapi.repository;
+
+import com.gts.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
