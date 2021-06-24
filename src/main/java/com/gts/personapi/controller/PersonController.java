b67c54c11584b5e18418988c8d08202c72e62bb4
gtsiqueira <gildotsiqueira@gmail.com>
2021-06-24 18:14:09 -0300

    aplicando melhoria no construtor

--- src/main/java/com/gts/personapi/controller/PersonController.java
+++ src/main/java/com/gts/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.gts.personapi.dto.request.PersonDTO;
 import com.gts.personapi.dto.response.MessageResponseDTO;
 import com.gts.personapi.exception.PersonNotFoundException;
 import com.gts.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
