ae21f626db3bd6da6952691127f65b5a3daef5eb
gtsiqueira <gildotsiqueira@gmail.com>
2021-06-24 16:04:59 -0300

    Correcoes do lab

--- src/main/java/com/gts/personapi/service/PersonService.java
+++ src/main/java/com/gts/personapi/service/PersonService.java
@@ -1,27 +1,42 @@
 package com.gts.personapi.service;
 
+import com.gts.personapi.dto.request.PersonDTO;
 import com.gts.personapi.dto.response.MessageResponseDTO;
 import com.gts.personapi.entity.Person;
+import com.gts.personapi.mapper.PersonMapper;
 import com.gts.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
